7eed9384f91cb8a8de4f9a738e8093b58088b8eb
Mark <marksnjoroge@gmail.com>
2022-08-02 13:25:51 +0200

    updated LoRaDongle code to working implementation of led flicker test

--- SOC/main/LoRaDongle.py
+++ SOC/main/LoRaDongle.py
@@ -1,141 +1,282 @@
 #!/usr/bin/env python3
 
-from migen import *
+# This file is Copyright (c) 2019 Sean Cross <sean@xobs.io>
+# This file is Copyright (c) 2018 David Shah <dave@ds0.me>
+# This file is Copyright (c) 2020 Piotr Esden-Tempski <piotr@esden.net>
+# License: BSD
 
+# This target was originally based on the Fomu target.
+
+# This variable defines all the external programs that this module
+# relies on.  lxbuildenv reads this variable in order to ensure
+# the build will finish without exiting due to missing third-party
+# programs.
+
+import argparse
+from os import path
+
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
 from migen.genlib.io import CRG
 
-from platform import Platform
-
-
-from litex.soc.integration.soc_core import *
-from litex.soc.integration.builder import *
 from litex.soc.cores.ram import Up5kSPRAM
+#from litex.soc.cores.spi_flash import SpiFlash
+from litex.soc.cores.clock import iCE40PLL
+from litex.soc.integration.soc_core import SoCCore
+from litex.soc.integration.builder import Builder, builder_argdict, builder_args
+from litex.build.lattice.programmer import IceStormProgrammer
+from litex.soc.integration.soc_core import soc_core_argdict, soc_core_args
+from litex.soc.integration.doc import AutoDoc
 from litex.soc.integration.soc import SoCRegion
-from litex.soc.interconnect.csr import *
+
+
+from Platform.LoRaDongle import Platform
+
+from litex.soc.cores.uart import UARTWishboneBridge
 from litex.soc.cores import gpio
+from litex.soc.cores.spi import SPIMaster
+
+import litex.soc.doc as lxsocdoc
 
 
-from litex.soc.doc import generate_docs
+# CRG ----------------------------------------------------------------------------------------------
 
 
+class _CRG(Module, AutoDoc):
+    """LoRaDongle Clock Resource Generator, pulled from Icebreaker
 
-kB = 1024
-mB = 1024*kB
+    The system is clocked by the external 50MHz clock. But if a sys_clk_freq is set to a value
+    that is different from the default 50MHz we will feed it through the PLL block and try to
+    generate a clock as close as possible to the selected frequency.
+    """
+    def __init__(self, platform, sys_clk_freq):
+        self.clock_domains.cd_sys = ClockDomain()
+        self.clock_domains.cd_por = ClockDomain()
+
+        # # #
+
+        # Clocks
+        clk = platform.request("clk")
+        if sys_clk_freq == 50e6:
+            self.comb += self.cd_sys.clk.eq(clk)
+        else:
+            self.submodules.pll = pll = iCE40PLL(primitive="SB_PLL40_PAD")
+            pll.register_clkin(clk, 50e6)
+            pll.create_clkout(self.cd_sys, sys_clk_freq, with_reset=False)
+        platform.add_period_constraint(self.cd_sys.clk, 1e9 / sys_clk_freq)
+
+        # Power On Reset
+        self.reset = Signal()
+        por_cycles = 4096
+        por_counter = Signal(log2_int(por_cycles), reset=por_cycles - 1)
+        self.comb += self.cd_por.clk.eq(self.cd_sys.clk)
+        platform.add_period_constraint(self.cd_por.clk, 1e9 / sys_clk_freq)
+        self.sync.por += If(por_counter != 0, por_counter.eq(por_counter - 1))
+        self.comb += self.cd_sys.rst.eq(por_counter != 0)
+        self.specials += AsyncResetSynchronizer(self.cd_por, self.reset)
 
 
+# BaseSoC ------------------------------------------------------------------------------------------
 
-# Create our soc (fpga description)
 class BaseSoC(SoCCore):
+    """A SoC on LoRaDongle, optionally with a softcore CPU"""
 
-    #SoCCore.mem_map = {
-    #    "sram":             0x10000000,
-    #    "spiflash":         0x20000000,
-    #    "csr":              0xf0000000,
-    #}
+    # Statically-define the memory map, to prevent it from shifting across various litex versions.
+    SoCCore.mem_map = {
+        "sram":             0x10000000,
+        "spiflash":         0x20000000,
+        "csr":              0xf0000000,
+        "vexriscv_debug":   0xf00f0000,
+    }
+
+    def __init__(self, debug, flash_offset, sys_clk_freq, **kwargs):
+        """Create a basic SoC for LoRaDongle.
+
+        Create a basic SoC for LoRaDongle.  The `sys` frequency will run at 12 MHz.
+
+        Returns:
+            Newly-constructed SoC
+        """
+        platform = Platform()
+
+        # Set cpu name and variant defaults when none are provided
+        if "cpu_variant" not in kwargs and "cpu_type" == "vexriscv":
+            if debug:
+                kwargs["cpu_variant"] = "lite+debug"
+            else:
+                kwargs["cpu_variant"] = "lite"
+
+        # Force the SRAM size to 0, because we add our own SRAM with SPRAM
+        kwargs["integrated_sram_size"] = 0
+        kwargs["integrated_rom_size"]  = 0
+
+        kwargs["csr_data_width"] = 32
+
+        # Set CPU reset address
+        kwargs["cpu_reset_address"] = self.mem_map["spiflash"] + flash_offset
+
+        # Select "crossover" as soc uart instead of "serial"
+        # We have to make that selection before calling the parent initializer
+        if debug:
+            kwargs["uart_name"]   = "crossover"
+
+        # SoCCore
+        SoCCore.__init__(self, platform, sys_clk_freq, **kwargs)
+
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
+        #self.submodules.crg = CRG(platform.request("clk"), ~platform.request("cpu_reset"))
 
 
-    def __init__(self, platform):
-        sys_clk_freq = int(50e6)
-
-        # Clock Reset Generation
-        self.submodules.crg = CRG(platform.request("clk50"))
-
-        # SoC with CPU
-        SoCCore.__init__(self, platform,
-            cpu_type                 = "serv",
-            clk_freq                 = sys_clk_freq,
-            ident                    = "LiteX CPU Test SoC on LoRaDongle", ident_version=True,
-            integrated_rom_size      = 0,
-            integrated_sram_size     = 0)	#Disable Integrated ROM/SRAM since too large for iCE40 and UP5k has specific SPRAM
-
-
-
-        # 128KB SPRAM (used as 64kB SRAM / 64kB RAM) -----------------------------------------------
-        # Soruced from litex_boards/icebreaker.py
-        #self.submodules.spram = Up5kSPRAM(size=128*kB)
-        #self.bus.add_slave("psram", self.spram.bus, SoCRegion(size=128*kB))
-        #self.bus.add_region("sram", SoCRegion(
-        #        origin = self.bus.regions["psram"].origin + 0*kB,
-        #        size   = 64*kB,
-        #        linker = True)
-        #)
-        #if not self.integrated_main_ram_size:
-        #    self.bus.add_region("main_ram", SoCRegion(
-        #        origin = self.bus.regions["psram"].origin + 64*kB,
-        #        size   = 64*kB,
-        #        linker = True)
-        #    )
-
-
-        # Sourced from icebreaker example
+        # UP5K has single port RAM, which is a dedicated 128 kilobyte block.
         # Use this as CPU RAM.
-        spram_size = 128 * kB
+        spram_size = 128 * 1024
         self.submodules.spram = Up5kSPRAM(size=spram_size)
         self.register_mem("sram", self.mem_map["sram"], self.spram.bus, spram_size)
 
 
+        #spi_clk = platform.request("spi_clk")       # CLock for SPI SIgnals
+
+
         # SPI Flash --------------------------------------------------------------------------------
-        from litespi.modules import N25Q032A            #will update to new flash :)
+        from litespi.modules import N25Q032A
         from litespi.opcodes import SpiNorFlashOpCodes as Codes
+        #from litespi.phy.generic import LiteSPIPHY
+        #spi_flash_pads = platform.request("spiflash")
+        #spi_flash_pads.clk = Signal()
+        #spiflash_phy = LiteSPIPHY(spi_flash_pads, N25Q032A(Codes.READ_1_1_1), device=self.platform.device, default_divisor=1)
+        #setattr(self.submodules, name + "_phy",  spiflash_phy)
+        #self.add_spi_flash(phy=spiflash_phy,mode="1x", module=N25Q032A(Codes.READ_1_1_1), with_master=False)
         self.add_spi_flash(mode="1x", module=N25Q032A(Codes.READ_1_1_1), with_master=False)
+
 
         # Add ROM linker region --------------------------------------------------------------------
         self.bus.add_region("rom", SoCRegion(
-            origin = self.bus.regions["spiflash"].origin + 0x20000, #change the offset value to a callable value, eg: bios_flash_offset
-            size   = 32*kB,
+            origin = self.mem_map["spiflash"] + flash_offset,
+            size   = 8*1024*1024,  # size of rom currently too big for current flash
             linker = True)
         )
-        self.cpu.set_reset_address(self.bus.regions["rom"].origin)
 
+        # In debug mode, add a UART bridge.  This takes over from the normal UART bridge,
+        # however you can use the "crossover" UART to communicate with this over the bridge.
+        if debug:
+            self.submodules.uart_bridge = UARTWishboneBridge(platform.request("serial"), sys_clk_freq, baudrate=115200)
+            self.add_wb_master(self.uart_bridge.wishbone)
 
-
+        #platform.add_extension(break_off_pmod)
 
         # Leds
         user_leds = Cat(*[platform.request("user_led", i) for i in range(2)])
         self.submodules.leds = gpio.GPIOOut(user_leds)
         self.add_csr("leds")
 
+         
+        #####LoRa Pins#####
+        #Busy Input Pin
+        self.submodules.lora_busy = gpio.GPIOIn(platform.request("lora_busy"))
+        self.add_csr("lora_busy")
+
+        #Output Pins
+        #Combined Register names - differentiation of pins in software
+        lora_config = Cat(*[platform.request("lora_config", i) for i in range(3)])
+        self.submodules.lora_config = gpio.GPIOOut(lora_config)
+        self.add_csr("lora_config")
+
+        # SPI
+        #spi_pads = platform.lookup_request("clk")
+        #spi_pads = platform.request("spi_bus", 0)
+        #spi_bus_pads = platform.request("spi_bus")
+        #spi_bus_pads.clk = Signal()
+        #self.submodules.SPI = SPIMaster(spi_bus_pads,
+        self.submodules.SPI = SPIMaster(platform.request("spi_bus", 0),
+            data_width = 8,
+            sys_clk_freq = sys_clk_freq,
+            spi_clk_freq = 8e6)
+        self.add_csr("spi_bus")
 
 
-# Flash -------------------------------------------------------------------------------------------
-def flash(build_dir, build_name, flash_offset):
-    from litex.build.lattice.programmer import IceStormProgrammer
+        #self.comb += spi_clk.eq(spi_flash_pads.clk | spi_bus_pads.clk) #Connecting SPI Clock to both SPIMaster and SPIFlash
+        #self.comb += spi_clk.eq(spi_flash_pads.clk)
+        #self.comb += spi_clk.eq(spi_bus_pads.clk)
+
+
+    def set_yosys_nextpnr_settings(self, nextpnr_seed=0, nextpnr_placer="heap"):
+        """Set Yosys/Nextpnr settings by overriding default LiteX's settings.
+        Args:
+            nextpnr_seed   (int): Seed to use in Nextpnr
+            nextpnr_placer (str): Placer to use in Nextpnr
+        """
+        assert hasattr(self.platform.toolchain, "yosys_template")
+        assert hasattr(self.platform.toolchain, "build_template")
+        self.platform.toolchain.yosys_template = [
+            "{read_files}",
+            "attrmap -tocase keep -imap keep=\"true\" keep=1 -imap keep=\"false\" keep=0 -remove keep=0",
+            # Use "-relut -dffe_min_ce_use 4" to the synth_ice40 command. The "-reult" adds an additional
+            # LUT pass to pack more stuff in, and the "-dffe_min_ce_use 4" flag prevents Yosys from
+            # generating a Clock Enable signal for a LUT that has fewer than 4 flip-flops. This increases
+            # density, and lets us use the FPGA more efficiently.
+            "synth_ice40 -json {build_name}.json -top {build_name} -relut -abc2 -dffe_min_ce_use 4 -relut",
+        ]
+        self.platform.toolchain.build_template = [
+            "yosys -q -l {build_name}.rpt {build_name}.ys",
+            "nextpnr-ice40 --json {build_name}.json --pcf {build_name}.pcf --asc {build_name}.txt" +
+            " --pre-pack {build_name}_pre_pack.py --{architecture} --package {package}" +
+            " --seed {}".format(nextpnr_seed) +
+            " --placer {}".format(nextpnr_placer),
+            # Disable final deep-sleep power down so firmware words are loaded onto softcore's address bus.
+            "icepack -s {build_name}.txt {build_name}.bin"
+        ]
+
+
+# Flash --------------------------------------------------------------------------------------------
+
+def flash(build_dir, build_name, bios_flash_offset):
+    #from litex.build.lattice.programmer import IceStormProgrammer
     prog = IceStormProgrammer()
-    prog.flash(flash_offset, f"{build_dir}/software/bios/bios.bin")
-    prog.flash(0x00000000, f"{build_dir}/gateware/{build_name}.bin")
-
+    #prog.flash(bios_flash_offset, f"{build_dir}/software/bios/bios.bin")
+    prog.flash(0x00000000,        f"{build_dir}/gateware/{build_name}.bin")
 
 # Build --------------------------------------------------------------------------------------------
-def main():
-    parser = argparse.ArgumentParser(description="Litex SoC on LoRaDongle")
-    parser.add_argument("--flash-offset", default=0x20000, help="Boot offset in SPI Flash")
-    parser.add_argument("--flash", action="store_true", help="Load bitstream")
 
+def main():
+    parser = argparse.ArgumentParser(description="LiteX SoC on LoRaDongle")
+    parser.add_argument("--build", action="store_true", help="Build SoC")
+    parser.add_argument("--flash-offset", default=0x20000, help="Boot offset in SPI Flash")
+    parser.add_argument("--sys-clk-freq", type=float, default=21e6, help="Select system clock frequency")
+    parser.add_argument("--nextpnr-seed", default=0, help="Select nextpnr pseudo random seed")
+    parser.add_argument("--nextpnr-placer", default="heap", choices=["sa", "heap"], help="Select nextpnr placer algorithm")
+    parser.add_argument("--debug", action="store_true", help="Enable debug features. (UART has to be used with the wishbone-tool.)")
+    parser.add_argument("--document-only", action="store_true", help="Do not build a soc. Only generate documentation.")
+    parser.add_argument("--flash", action="store_true", help="Load bitstream")
     builder_args(parser)
     soc_core_args(parser)
-
     args = parser.parse_args()
 
-    platform = Platform()
-    # Create SOC
-    soc = BaseSoC(platform)
+    # Create the SOC
+    soc = BaseSoC(debug=args.debug, flash_offset=args.flash_offset, sys_clk_freq=int(args.sys_clk_freq), **soc_core_argdict(args))
+    soc.set_yosys_nextpnr_settings(nextpnr_seed=args.nextpnr_seed, nextpnr_placer=args.nextpnr_placer)
 
+    # Configure command line parameter defaults
+    # Don't build software -- we don't include it since we just jump to SPI flash.
+    builder_kwargs = builder_argdict(args)
+    #builder_kwargs["compile_software"] = False
 
-    builder = Builder(soc, output_dir="build", csr_csv="csr.csv")
-    vns = builder.build(build_name="LoRaDongle")
-    soc.do_exit(vns)
-    generate_docs(soc, "build/documentation")
+    if args.document_only:
+        builder_kwargs["compile_gateware"] = False
+    #if builder_kwargs["csr_svd"] is None:
+    #    builder_kwargs["csr_svd"] = "../rust/icebesoc-pac/iCEBESOC.svd"
+    #if builder_kwargs["memory_x"] is None:
+    #    builder_kwargs["memory_x"] = "../rust/icebesoc-pac/memory.x"
 
+    # Create and run the builder
+    builder = Builder(soc, **builder_kwargs)
+    if args.build:
+        builder.build()
+        lxsocdoc.generate_docs(soc, "build/documentation/", project_name="LoRaDongle LiteX Riscv Example SOC")
 
-
+    # If requested load the resulting bitstream onto the LoRaDongle
     if args.flash:
-        flash(builder.output_dir, soc.build_name, args.flash_offset)
-
+        IceStormProgrammer().flash(0x00000000, path.join(builder.gateware_dir,"{}.bin".format(soc.build_name)))
+        #flash(builder.output_dir, soc.build_name, args.flash_offset)
 
 if __name__ == "__main__":
     main()
-
-
-#command to program software
-
-# iceprog -o 0x000400 demo.bin
--- SOC/main/firmware/src/main.c
+++ SOC/main/firmware/src/main.c
@@ -1,21 +1,377 @@
 #include <generated/csr.h>
 #include <time.h>
+#include <libbase/console.h>
+#include <libbase/uart.h>
+#include <stdio.h>
+#include <string.h>
+#include <spi.h>
+#include <sx126x.h>
+#include "sx126x_hal.h"
+
+#define UART_EV_TX 0x1
+#define UART_EV_RX 0x2
+
+
+// Radio Settings
+#define RX_TIMEOUT_US 200000
+
+/* Set this flag to '1' to use the LoRa modulation or to '0' to use FSK modulation */
+#define USE_MODEM_LORA  1
+#define USE_MODEM_FSK   !USE_MODEM_LORA
+ 
+#define RF_FREQUENCY                                    869524963 // hz // 868000000 // Hz
+#define TX_OUTPUT_POWER                                 14        // 14 dBm
+ 
+#if USE_MODEM_LORA == 1
+ 
+    #define LORA_BANDWIDTH                              SX126X_LORA_BW_125         // [0: 125 kHz,
+                                                                  					//  1: 250 kHz,
+                                                                 				 //  2: 500 kHz,
+                                                                				  //  3: Reserved]
+    #define LORA_SPREADING_FACTOR                       SX126X_LORA_SF7         // [SF7..SF12]
+    #define LORA_LOWDATARATEOPTIMIZE                    0
+    #define LORA_CODINGRATE                             SX126X_LORA_CR_4_6         // [1: 4/5,
+																					//  2: 4/6,
+																					//  3: 4/7,
+																					//  4: 4/8]
+    #define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+    #define LORA_SYMBOL_TIMEOUT                         5         // Symbols
+    #define LORA_HEADER_TYPE                            SX126X_LORA_PKT_EXPLICIT
+    #define LORA_FHSS_ENABLED                           false  
+    #define LORA_NB_SYMB_HOP                            4     
+    #define LORA_IQ                                     false  //LORA_IQ_NORMAL
+    #define LORA_CRC_MODE                               false  //LORA_CRC_OFF
+
+#endif
+
+#define RX_TIMEOUT_VALUE                                3500      // in ms
+#define BUFFER_SIZE                                     64        // Define the payload size here
+
+
+
+//SX126x Context Stuff :)  Need a configuration function obviously
+
+
+typedef struct{
+	sx126x_pkt_type_t packetType;
+	int8_t txPower;
+	sx126x_ramp_time_t txRampTime;
+	sx126x_mod_params_lora_t  modParams;
+	sx126x_pkt_params_lora_t packetParams;
+	uint32_t rfFrequency;
+    uint16_t irqTx;
+    uint16_t irqRx;
+    uint32_t txTimeout;
+    uint32_t rxTimeout;
+}RadioConfig_t;
+RadioConfig_t context; 
+
+// Sx126x user function Declarations
+void RadioInit(void);
+sx126x_status_t BufferReadWrite(void);
+void SetConfiguration(RadioConfig_t *config);
+void ConfigureGeneralRadio(RadioConfig_t *config);
+void ConfigureTx(RadioConfig_t *config);
+void PrepareBuffer(RadioConfig_t *config);
+void transmit(RadioConfig_t *config);
+
+
+void SetAntSW(void);
+void ClearAntSW(void);
+void ToggleAntSW(void);
+void ToggleDio1(void);
+
+void flicker(void);
+
+/*
+	Uart functions for interacting with Console
+*/
+static char *readstr(void)
+{
+	char c[2];
+	static char s[64];
+	static int ptr = 0;
+
+	if(readchar_nonblock()) {
+		c[0] = getchar();
+		c[1] = 0;
+		switch(c[0]) {
+			case 0x7f:
+			case 0x08:
+				if(ptr > 0) {
+					ptr--;
+					fputs("\x08 \x08", stdout);
+				}
+				break;
+			case 0x07:
+				break;
+			case '\r':
+			case '\n':
+				s[ptr] = 0x00;
+				fputs("\n", stdout);
+				ptr = 0;
+				return s;
+			default:
+				if(ptr >= (sizeof(s) - 1))
+					break;
+				fputs(c, stdout);
+				s[ptr] = c[0];
+				ptr++;
+				break;
+		}
+	}
+
+	return NULL;
+}
+
+static char *get_token(char **str)
+{
+	char *c, *d;
+
+	c = (char *)strchr(*str, ' ');
+	if(c == NULL) {
+		d = *str;
+		*str = *str+strlen(*str);
+		return d;
+	}
+	*c = 0;
+	d = *str;
+	*str = c+1;
+	return d;
+}
+
+/* 
+terminal like thing
+*/
+static void prompt(void) {
+	printf("\e[92;1mLoRaDongle-SoC\e[0m> ");
+}
+
+
+/* 
+	Console Commands
+*/
+
+static void reboot_cmd(void){
+	ctrl_reset_soc_rst_write(0x1);
+}
+
+
+static void help(void) {
+	puts("\nLoRaDongle SOC Test");
+	puts("Available Commands:");
+	puts("help			- Show this command");
+	puts("reboot			- Reboots CPU");
+	puts("spi_test		- SPI Loopback Test");
+	puts("led_demo		- Flickers LEDs");
+}
+
+static void console_service(void) {
+	char *str;
+	char *token;
+
+	str = readstr();
+	if(str == NULL) return;
+	token = get_token(&str);
+	if(strcmp(token, "help") == 0)
+		help();
+	else if(strcmp(token, "reboot") == 0)
+		reboot_cmd();
+    else if (strcmp(token, "led_demo") == 0)
+		flicker();
+	else if (strcmp(token, "spi_test") == 0)
+		test_loopback(0xa5);
+
+#ifdef CSR_LEDS_BASE
+
+#endif
+
+	prompt();
+}
+
 
 int main(void) {
-    // time_init configures timer0 so we can use elapsed()
     time_init();
-    uint32_t x = 0;
-    uint32_t buf = 0;
-    
-    while (1) {
-        // Write new LED output
-        if (x == 2) x = 1;
-        else x = 2;
-        msleep(80);
+    uart_init();
+	//RadioInit();
+	//SetConfiguration(&context);
+	//ConfigureGeneralRadio(&context);
+    help();
+    prompt();
+    flicker();
+    //int i = 0;
 
-        leds_out_write(x | buf << 2);
-        uart_rxtx_write('a');
+    while (1) {
+        console_service();
+		//printf("Transmitting: %d\n", i++);
+		//PrepareBuffer(&context);
+		//ConfigureTx(&context);
+		//transmit(&context);
+		//msleep(1000);       
     }
 
     return 0;
+}
+
+
+// Sx126x Test Functions
+void RadioInit(void){
+	/*
+		From Nucleo Code:
+		calibration parameters #what is that? #TODO, only used with OPT apparently
+		
+		reset()
+		
+		irq init #TODO - initialises spi, sets busy as open drain pullnone (inverse logic), assigns dio1 callback
+		looks like irq_init just initiliasises the pins. Busy for busy stuff, dio1 for dio stuff, and also spi obvi
+RX_TIMEOUT_US
+
+		setPollingMode() //aookucaktion calls process irqs as opposed to driver automatically doing it.
+		
+		turn on antenna switch (set pin to high basically), always high except in sleep mode
+		this is the use in the other switch though not mine. mine i'd need to set it 1 or 0 in order to choose the rf path to follow :)
+		
+		Set Dio2AsRfSwitchCtrl = true  # available in sx126x.c, tell radio to control rf switch
+
+		operating mode = Mode_stby_rc # what is that?? #TODO basically a way for the mcu to know track what operating mode the radio is in
+		setPacketType to lora # available in sx126x.c
+
+		configure for public network/private network
+	*/
+
+	sx126x_reset(&context);
+	
+	sx126x_wakeup(&context);
+	sx126x_set_standby(&context, 0x00); // hard coded but can be used as a variable
+
+	ClearAntSW();
+	sx126x_set_dio2_as_rf_sw_ctrl(&context, true);  
+
+	sx126x_set_pkt_type(&context, SX126X_PKT_TYPE_LORA);
+}
+
+void SetConfiguration(RadioConfig_t *config){
+	config->irqRx = SX126X_IRQ_RX_DONE | SX126X_IRQ_TIMEOUT;
+    config->irqTx = SX126X_IRQ_TX_DONE | SX126X_IRQ_TIMEOUT;
+    config->rfFrequency = RF_FREQUENCY;
+    config->txTimeout = 0;
+    config->rxTimeout = (uint32_t)(RX_TIMEOUT_US / 15.625);
+    config->txPower = TX_OUTPUT_POWER;
+    config->txRampTime = SX126X_RAMP_200_US;
+	#if USE_MODEM_LORA == 1
+        config->packetType = SX126X_PKT_TYPE_LORA;
+        config->modParams.bw = LORA_BANDWIDTH;
+        config->modParams.cr = LORA_CODINGRATE;
+        config->modParams.ldro = LORA_LOWDATARATEOPTIMIZE;
+        config->modParams.sf = LORA_SPREADING_FACTOR;
+        config->packetParams.crc_is_on = LORA_CRC_MODE; //might be cause for error
+        config->packetParams.header_type = LORA_HEADER_TYPE;
+        config->packetParams.invert_iq_is_on = LORA_IQ; // might be cause for error
+        config->packetParams.pld_len_in_bytes = BUFFER_SIZE;
+        config->packetParams.preamble_len_in_symb = LORA_PREAMBLE_LENGTH;
+	#endif
+}
+
+
+void ConfigureGeneralRadio(RadioConfig_t *config){
+	sx126x_set_pkt_type(config, config->packetType);
+	sx126x_set_lora_pkt_params(config, &config->packetParams);
+	sx126x_set_lora_mod_params(config, &config->modParams);
+	sx126x_set_rf_freq(config, config->rfFrequency);
+	sx126x_set_tx_params(config, config->txPower, config->txRampTime);
+	// set interrupt mode
+}
+
+void ConfigureTx(RadioConfig_t *config){
+	sx126x_set_dio_irq_params(config, config->irqTx, config->irqTx, SX126X_IRQ_NONE, SX126X_IRQ_NONE);
+}
+
+void PrepareBuffer(RadioConfig_t *context){
+	uint8_t offset = 0x0;
+	uint8_t size = 4;
+	uint8_t WriteMessage[4] = {'P', 'I', 'N', 'G'};
+	uint8_t *pWrite = WriteMessage;
+
+	sx126x_write_buffer(&context, offset, pWrite, size);
+
+}
+
+void transmit(RadioConfig_t *config){
+	//set fs first?
+	sx126x_set_tx(config, config->txTimeout);
+}
+
+
+sx126x_status_t BufferReadWrite(void){
+	uint8_t offset = 0x2;
+	uint8_t size = 4;
+	uint8_t WriteMessage[4] = {'P', 'I', 'N', 'G'};
+	uint8_t ReadMessage[4];
+
+	uint8_t *pWrite = WriteMessage;
+	uint8_t *pRead = ReadMessage;
+
+	sx126x_read_buffer(&context, offset, pRead, size);
+	printf("The read data is: \n");
+	
+	for (uint8_t i = 0; i < size; i++) {
+        printf("%x\n" ,*pRead++);      // Might bring an error because i'm point to a pointer
+    }    
+
+	printf("Writing New Buffer\n");
+
+	sx126x_write_buffer(&context, offset, pWrite, size);
+	printf("Reading back data written\n");
+
+	sx126x_read_buffer(&context, offset, pRead, size);
+	printf("The read data is: \n");
+	
+	for (uint8_t i = 0; i < size; i++) {
+        printf("%x\n" , *pRead++);      
+    }    
+
+	return 0;
+}
+
+
+
+// these functions are temporarily here in case i need them, to turn on/off the pins
+void ToggleAntSW(void){
+	const uint8_t ANT_SW = 1;
+	//lora_config_out_write(lora_config_out_read() | (1 << ANT_SW ) );  // setting to 1
+	lora_config_out_write(lora_config_out_read() ^ (1 << ANT_SW ) );
+}
+
+void SetAntSW(void){
+	const uint8_t ANT_SW = 1;
+	lora_config_out_write(lora_config_out_read() | (1 << ANT_SW ) );  // setting to 1
+}
+
+void ClearAntSW(void){
+	const uint8_t ANT_SW = 1;
+	lora_config_out_write( lora_config_out_read() & ~(1 << ANT_SW) );  // set reset pin to 0
+
+}
+
+void ToggleDio1(void){
+	const uint8_t DIO1 = 0;
+    //lora_config_out_write(lora_config_out_read() | (1 << DIO1) );  // setting to 1
+	lora_config_out_write(lora_config_out_read() ^ (1 << DIO1 ) );
+
+}
+
+
+// Flickers the two tx and Rx LEDs
+void flicker(void) {
+        uint32_t x = 0;
+        uint32_t buf = 0;
+        // Write new LED output
+        for(int i = 0; i<10; i++) {
+            if (x == 2) x = 1;
+            else x = 2;
+            msleep(80);
+
+            leds_out_write(x | buf << 2);
+        }
+        leds_out_write(0);
 }
